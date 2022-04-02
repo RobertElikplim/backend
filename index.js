cf986b282e1d88a8c1903ad59b6cdbd188dc7fe0
RobertEli <57449733+RobertElikplim@users.noreply.github.com>
2022-04-02 15:28:58 -0500

    changed port

--- index.js
+++ index.js
@@ -8,6 +8,8 @@ import Router from "./routes/routes.js";
 // init express
 const app = express();
 
+const PORT = process.env.PORT || 3000;
+
 // use express json
 app.use(express.json());
 
@@ -18,4 +20,4 @@ app.use(cors());
 app.use(Router);
 
 
-app.listen(3000, () => console.log('Server running at http://localhost:3000')); 
+app.listen(port, () => console.log('Server running at http://localhost:3000')); 
